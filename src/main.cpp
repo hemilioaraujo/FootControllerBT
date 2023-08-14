eb2d37add20cac73c43ef336c9f9df0d8e3a1652
Hemilio Melo <hemilioaraujo@gmail.com>
2023-08-14 19:32:43 -0300

    feat: add reset button

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,35 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
 
+void press_reset();
 void press_up();
 void press_down();
 void release();
 void press(uint8_t button);
 
+const int BUTTON_RESET = 12;
 const int BUTTON_UP = 13;
 const int BUTTON_DOWN = 15;
+const int LED_CONN = 16;
 const int DELAY = 300;
+
+const char DEVICE_NAME[12]="PAGE_TURNER";
+const char DEVELOPER[5]="HLAM";
 
 int buttonState = 0;
 
-BleKeyboard bleKeyboard("PAGE_TURNER", "HLAM", 100);
+BleKeyboard bleKeyboard(DEVICE_NAME, DEVELOPER, 100);
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(BUTTON_RESET, INPUT);
     pinMode(BUTTON_UP, INPUT);
     pinMode(BUTTON_DOWN, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_CONN, OUTPUT);
+
     Serial.begin(115200);
-    Serial.println("Starting BLE work!");
+    Serial.println("Starting !");
     bleKeyboard.begin();
 }
 
@@ -30,9 +39,16 @@ void loop()
 {
     buttonState = 0;
 
+    buttonState = digitalRead(BUTTON_RESET);
+
+    if (buttonState == HIGH)
+    {
+        press_reset();
+    }
+
     if (bleKeyboard.isConnected())
     {
-        digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(LED_CONN, HIGH);
 
         buttonState = digitalRead(BUTTON_UP);
 
@@ -50,7 +66,7 @@ void loop()
     }
     else
     {
-        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(LED_CONN, LOW);
     }
 }
 
@@ -64,6 +80,13 @@ void press_down()
 {
     Serial.println("DOWN");
     press(KEY_DOWN_ARROW);
+}
+
+void press_reset()
+{
+    Serial.println("RESET");
+    delay(DELAY);
+    esp_restart();
 }
 
 void release()
